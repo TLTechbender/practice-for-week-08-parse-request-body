2f92cfe6647cc9fb82c8477c7fa6702e02a22e72
TLTechbender <myjoyhasarrived2004@gmail.com>
2023-02-22 02:49:54 +0100

    I have completely forgotten logic, I don't even know  how this is working

--- parse-body.js
+++ parse-body.js
@@ -1,27 +1,67 @@
+
 function firstStep(input) {
   // Your code here
+  return input.split('&');
 }
 
 function secondStep(input) {
   // Your code here
+  const paired = input.map(el => {
+    let pair = el.split('=');
+    return pair;
+  });
+
+  return paired;
 }
 
 function thirdStep(input) {
   // Your code here
+  const replaced = input.map(el => {
+    const newEl = el.map(str => {
+      return str.replace(/\+/g, " ");
+    });
+
+    return newEl;
+  });
+
+  return replaced;
 }
 
 function fourthStep(input) {
   // Your code here
+  const decoded = input.map(el => {
+    const newEl = el.map(str => {
+      return decodeURIComponent(str);
+    });
+
+    return newEl;
+  });
+
+  return decoded;
 }
 
 function fifthStep(input) {
   // Your code here
+  const body = input.reduce((sorted, pair) => {
+    return {
+      ...sorted,
+      [pair[0]]: pair[1],
+    };
+  }, {});
+
+  return body;
 }
 
 function parseBody(str) {
   // Your code here
-}
+  let input = firstStep(str);
+  input = secondStep(input);
+  input = thirdStep(input);
+  input = fourthStep(input);
+  input = fifthStep(input);
 
+  return input;
+}
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
 
